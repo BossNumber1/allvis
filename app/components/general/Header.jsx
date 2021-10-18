e0e5690d5db284be9be4ac3c104bfe39f1810f9f
BossNumber1 <dimaang23@gmail.com>
2021-10-18 07:06:01 +0300

    Upgrade header

--- app/components/general/Header.jsx
+++ app/components/general/Header.jsx
@@ -4,15 +4,20 @@ export default function Header() {
     return (
         <div className={headerStyles.commonBlock}>
             <div>Ustinov Corp.</div>
+            <div className={headerStyles.centerBlock}>
+                <div>
+                    <a href="/contacts">Контакты</a>
+                </div>
+                <div>
+                    <a href="/about">О нас</a>
+                </div>
+                <div>
+                    <a href="/otherInfo">Другое</a>
+                </div>
+            </div>
             <div className={headerStyles.rightBlock}>
                 <div>
-                    <a href="/about">About</a>
-                </div>
-                <div>
-                    <a href="/contacts">Contacts</a>
-                </div>
-                <div>
-                    <a href="/otherInfo">Other info</a>
+                    <a href="/">На главную</a>
                 </div>
             </div>
         </div>
