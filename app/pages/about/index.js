503bb13dc9f0ac19ee82a152040985f1786fe616
BossNumber1 <dimaang23@gmail.com>
2021-10-18 07:30:40 +0300

    Filled in the page About

--- app/pages/about/index.js
+++ app/pages/about/index.js
@@ -1,9 +1,18 @@
 import { MainLayout } from "../../components/MainLayout";
+import aboutStyles from "../../styles/about.module.css";
 
 export default function About() {
     return (
         <MainLayout title="Cтраница о нас">
-            Добро пожаловать на страницу о нас
+            <div className={aboutStyles.bodyPage}>
+                <div>
+                    Я основал компанию Ustinov Corp. 14 октября 2021 года на
+                    самозанятое лицо, в данный момент. Возможно стану
+                    расширяться...
+                </div>
+                <div>Меня зовут Устинов Дмитрий Сергеевич.</div>
+                <div>Реквизиты...</div>
+            </div>
         </MainLayout>
     );
 }
