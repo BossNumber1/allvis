38862d166bb1731f630dc5deb808de027429648f
BossNumber1 <dimaang23@gmail.com>
2021-10-18 07:21:41 +0300

    Added contacts

--- app/pages/contacts/index.js
+++ app/pages/contacts/index.js
@@ -1,9 +1,20 @@
 import { MainLayout } from "../../components/MainLayout";
+import contacts from "../../styles/contacts.module.css";
 
 export default function Contacts() {
     return (
         <MainLayout title="Наши контакты">
-            Добро пожаловать на страницу контактов
+            <div className={contacts.bodyPage}>
+                <div>Привет :) Меня зовут Дмитрий. И мои контакты</div>
+                <div style={{ display: "flex" }}>
+                    <div style={{ margin: "0px 25px 0px 90px" }}>
+                        <a href="https://vk.com/moyustimov">ВКонтакте</a>
+                    </div>
+                    <div>
+                        <a href="https://t.me/moyustimov">Телеграмм</a>
+                    </div>
+                </div>
+            </div>
         </MainLayout>
     );
 }
