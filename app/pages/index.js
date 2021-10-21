946bc5d7592089cf7f8f5d174fe98a9941774eb1
BossNumber1 <dimaang23@gmail.com>
2021-10-21 13:06:56 +0300

    Added btn to connect

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,37 +4,69 @@ import { MainLayout } from "../components/MainLayout";
 export default function Home() {
     return (
         <MainLayout title="Cтартовая страница">
-            <div style={{ marginBottom: 30 }}>
-                Привет. <b>Создаю сайты-визитки</b> на заказ. Вот мои труды:
-            </div>
             <div className={homeStyles.exampleBCards}>
-                <div style={{ border: "1px solid black;" }}>
-                    <div>
-                        <a href="/#">
-                            <img src="https://impulse-design.com.ua/images/blog/primery-sajtov-vizitok/primery-sajtov-vizitok23-min.jpg"></img>
-                        </a>
-                    </div>
-                    <div style={{ textAlign: "center" }}>Сайт про гонки</div>
-                </div>
-                <div style={{ border: "1px solid black;" }}>
-                    <div>
-                        <a href="/#">
-                            <img src="https://impulse-design.com.ua/images/blog/primery-sajtov-vizitok/primery-sajtov-vizitok22-min.jpg"></img>
-                        </a>
-                    </div>
-                    <div style={{ textAlign: "center" }}>Сайт про ранчо</div>
-                </div>
-            </div>
-            <div style={{ textAlign: "center", marginTop: 32 }}>
-                <div>2-3 дня - 2000 рублей</div>
                 <div>
-                    <h1>Что получаете?</h1>
+                    <div
+                        style={{
+                            // marginBottom: 30,
+                            // marginLeft: 60,
+                            fontSize: "2.75rem",
+                            width: 650,
+                            // backgroundColor: "orange",
+                        }}
+                    >
+                        Создание сайтов-визиток на заказ...
+                    </div>
+                    <div className={homeStyles.firstblock}>
+                        <div
+                            style={{
+                                margin: "32px 0px",
+                            }}
+                        >
+                            За 2000 рублей вы получаете:
+                        </div>
+                        <ul>
+                            <li>Экономию времени и сил</li>
+                            <li>Качество</li>
+                        </ul>
+                        <button
+                            type="button"
+                            className="btn btn-dark mt-3"
+                            onClick={() =>
+                                (document.location.href =
+                                    "https://vk.com/moyustimov")
+                            }
+                        >
+                            Связаться
+                        </button>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        border: "1px solid black",
+                        position: "relative",
+                        // marginLeft: 50,
+                    }}
+                >
+                    <img
+                        src="https://impulse-design.com.ua/images/blog/primery-sajtov-vizitok/primery-sajtov-vizitok23-min.jpg"
+                        onClick={() => alert("terry222")}
+                    />
+                    {/* <div style={{ textAlign: "center" }}>Сайт про гонки</div> */}
+                </div>
+                <div
+                    style={{
+                        border: "1px solid black",
+                        position: "absolute",
+                        marginTop: 150,
+                        // marginRight: 50,
+                        // zIndex: 10,
+                        marginLeft: 730,
+                    }}
+                >
+                    <img src="https://impulse-design.com.ua/images/blog/primery-sajtov-vizitok/primery-sajtov-vizitok22-min.jpg" />
                 </div>
             </div>
-            <ul>
-                <li>Экономию времени и сил</li>
-                <li>Качество</li>
-            </ul>
         </MainLayout>
     );
 }
