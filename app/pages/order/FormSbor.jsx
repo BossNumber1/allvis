ad7ff1147ae9c69391eeb12ac66ee9cd8d545eeb
BossNumber1 <dimaang23@gmail.com>
2021-10-21 15:10:06 +0300

    up

--- app/pages/order/FormSbor.jsx
+++ app/pages/order/FormSbor.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import successIcon from "../../public/success.png";
+import Image from "next/image";
 
 function FormSbor() {
     const [name, setName] = React.useState("");
@@ -6,90 +8,101 @@ function FormSbor() {
     const [message, setMessage] = React.useState("");
     const [success, setSuccess] = React.useState(false);
     const [danger, setDanger] = React.useState(false);
+    const [disabledBtn, setDisabledBtn] = React.useState(false);
 
     let nameChange = (e) => {
         e.preventDefault();
         setName(e.target.value);
         setDanger(false);
+        setDisabledBtn(false);
     };
 
     let phoneChange = (e) => {
         e.preventDefault();
         setPhone(e.target.value);
         setDanger(false);
+        setDisabledBtn(false);
     };
 
     let messageChange = (e) => {
         e.preventDefault();
         setMessage(e.target.value);
         setDanger(false);
+        setDisabledBtn(false);
     };
 
     let submitHandler = (e) => {
         e.preventDefault();
 
         if (name !== "" && phone !== "" && message !== "") {
-            setSuccess(" форма заполнена, ура! ");
+            setSuccess("Заказ принят!");
+            setName("");
+            setPhone("");
+            setMessage("");
         } else {
-            setDanger(" не все поля заполнены ");
+            setDisabledBtn(true);
+            setDanger("Стоит заполнить все поля");
         }
     };
 
     return (
         <div className="ml-5 mr-5">
-            <div
-                className={
-                    success
-                        ? "alert alert-success"
-                        : danger && "alert alert-danger"
-                }
-            >
-                {success || danger}
-            </div>
-            <form>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">
-                        Имя
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="name"
-                        value={name}
-                        onChange={nameChange}
-                    />
+            <div className={danger && "alert alert-danger"}>{danger}</div>
+
+            {!success ? (
+                <form>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                            Имя
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="name"
+                            value={name}
+                            onChange={nameChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                            Телефон
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="phone"
+                            value={phone}
+                            onChange={phoneChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">
+                            Сообщение
+                        </label>
+                        <textarea
+                            className="form-control"
+                            name="message"
+                            value={message}
+                            onChange={messageChange}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-dark"
+                        onClick={submitHandler}
+                        disabled={disabledBtn && "disabled"}
+                    >
+                        Отправить
+                    </button>
+                </form>
+            ) : (
+                <div className="mr-5">
+                    <div>
+                        <Image src={successIcon} width={100} height={100} />
+                    </div>
+                    <div style={{ fontWeight: 700 }}>{success}</div>
                 </div>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">
-                        Телефон
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="phone"
-                        value={phone}
-                        onChange={phoneChange}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">
-                        Сообщение
-                    </label>
-                    <textarea
-                        className="form-control"
-                        name="message"
-                        value={message}
-                        onChange={messageChange}
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="btn btn-dark"
-                    onClick={submitHandler}
-                >
-                    Отправить
-                </button>
-            </form>
+            )}
         </div>
     );
 }
