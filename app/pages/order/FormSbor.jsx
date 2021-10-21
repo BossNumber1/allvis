130f746b93f4f12b39985cb6312b931c76317fbc
BossNumber1 <dimaang23@gmail.com>
2021-10-21 14:39:02 +0300

    Added data collection

--- app/pages/order/FormSbor.jsx
+++ app/pages/order/FormSbor.jsx
@@ -1,34 +1,73 @@
 import React from "react";
 
 function FormSbor() {
+    let submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log("name =", name, "phone =", phone, "message =", message);
+    };
+
+    const [name, setName] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    let nameChange = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    };
+
+    let phoneChange = (e) => {
+        e.preventDefault();
+        setPhone(e.target.value);
+    };
+    let messageChange = (e) => {
+        e.preventDefault();
+        setMessage(e.target.value);
+    };
+
     return (
         <div className="ml-5 mr-5">
             <form>
                 <div className="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Имя
+                    </label>
                     <input
                         type="text"
                         className="form-control"
-                        id="exampleInputEmail1"
-                        aria-describedby="emailHelp"
-                        placeholder="Имя"
+                        name="name"
+                        value={name}
+                        onChange={nameChange}
                     />
                 </div>
                 <div className="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Телефон
+                    </label>
                     <input
                         type="text"
                         className="form-control"
-                        id="exampleInputPassword1"
-                        placeholder="Телефон"
+                        name="phone"
+                        value={phone}
+                        onChange={phoneChange}
                     />
                 </div>
                 <div className="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">
+                        Сообщение
+                    </label>
                     <textarea
                         className="form-control"
-                        id="exampleInputPassword1"
-                        placeholder="Сообщение"
+                        name="message"
+                        value={message}
+                        onChange={messageChange}
                     />
                 </div>
-                <button type="submit" className="btn btn-dark">
+                <button
+                    type="submit"
+                    className="btn btn-dark"
+                    onClick={submitHandler}
+                >
                     Отправить
                 </button>
             </form>
