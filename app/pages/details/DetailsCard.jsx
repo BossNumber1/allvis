a480db8b52243508ee88af664dc4b02966b367dc
BossNumber1 <dimaang23@gmail.com>
2021-10-25 18:45:01 +0300

    upgrade card

--- app/pages/details/DetailsCard.jsx
+++ app/pages/details/DetailsCard.jsx
@@ -1,23 +1,57 @@
 import React from "react";
 
 function DetailsCard({ text }) {
+    const [isShow, setShow] = React.useState(false);
+
     return (
-        <div style={{ display: "flex" }}>
-            <div style={{ border: "none", margin: 0 }}>{text}</div>
-            <div
-                style={{
-                    border: "none",
-                    textAlign: "right",
-                    margin: 0,
-                }}
-            >
-                <img
-                    src="/down.png"
-                    alt="icon down"
-                    onClick={() => alert("terry222")}
-                />
-            </div>
-        </div>
+        <>
+            {!isShow ? (
+                <>
+                    <div
+                        style={{ display: "flex" }}
+                        onClick={() => setShow(!isShow)}
+                    >
+                        <div style={{ border: "none", margin: 0 }}>{text}</div>
+                        <div
+                            style={{
+                                border: "none",
+                                textAlign: "right",
+                                margin: 0,
+                            }}
+                        >
+                            <img
+                                src={!isShow ? "/down.png" : "/up.png"}
+                                alt="icon down"
+                            />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <div style={{ border: "none", margin: 0, padding: 0 }}>
+                    <div
+                        style={{ display: "flex" }}
+                        onClick={() => setShow(!isShow)}
+                    >
+                        <div style={{ border: "none", margin: 0 }}>{text}</div>
+                        <div
+                            style={{
+                                border: "none",
+                                textAlign: "right",
+                                margin: 0,
+                            }}
+                        >
+                            <img
+                                src={!isShow ? "/down.png" : "/up.png"}
+                                alt="icon down"
+                            />
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor: "rgb(245, 227, 196)" }}>
+                        details...
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
