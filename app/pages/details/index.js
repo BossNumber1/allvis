62be785ba50e56d070cfc4e74c71b3047a158a1e
BossNumber1 <dimaang23@gmail.com>
2021-10-26 15:32:56 +0300

    Made a responsive page Details and her upgraded

--- app/pages/details/index.js
+++ app/pages/details/index.js
@@ -3,32 +3,38 @@ import detailsStyles from "../../styles/details.module.css";
 import DetailsCard from "./DetailsCard";
 
 export default function OtherInfo() {
+    let dataList = [
+        {
+            id: "0",
+            text: "Next JS",
+            details:
+                "Next.js - это фреймворк, основанный на React, который позволяет создавать веб-приложения с улучшенной производительностью и улучшенным пользовательским опытом с помощью дополнительных функций предварительного рендеринга, таких как полноценный рендеринг на стороне сервера (SSR) и статическая генерация страниц (SSG).",
+        },
+        {
+            id: "1",
+            text: "Bootstrap",
+            details:
+                "Bootstrap — это открытый и бесплатный HTML, CSS и JS фреймворк, который используется веб-разработчиками для быстрой вёрстки адаптивных дизайнов сайтов и веб-приложений.",
+        },
+    ];
+
     return (
         <MainLayout title="Подробности">
-            <div style={{ fontSize: 25, fontWeight: 900, marginBottom: 30 }}>
-                Технологии
-            </div>
-            <div
-                style={{
-                    fontSize: 18,
-                    width: 400,
-                    textAlign: "center",
-                    marginBottom: 30,
-                }}
-            >
-                Мы используем один из лучших подходов к <b>быстрому</b> созданию{" "}
-                <b>качественного</b> продукта
-            </div>
-
-            <div className={detailsStyles.list}>
-                <DetailsCard
-                    text="Next JS"
-                    details="Next.js - это фреймворк, основанный на React, который позволяет создавать веб-приложения с улучшенной производительностью и улучшенным пользовательским опытом с помощью дополнительных функций предварительного рендеринга, таких как полноценный рендеринг на стороне сервера (SSR) и статическая генерация страниц (SSG)."
-                />
-                <DetailsCard
-                    text="Bootstrap"
-                    details="Bootstrap — это открытый и бесплатный HTML, CSS и JS фреймворк, который используется веб-разработчиками для быстрой вёрстки адаптивных дизайнов сайтов и веб-приложений."
-                />
+            <div className={detailsStyles.bodyPage}>
+                <div className={detailsStyles.title}>Технологии</div>
+                <div className={detailsStyles.description}>
+                    Мы используем один из лучших подходов к <b>быстрому</b>{" "}
+                    созданию <b>качественного</b> продукта
+                </div>
+                <div className={detailsStyles.list}>
+                    {dataList.map((el) => (
+                        <DetailsCard
+                            key={el.id}
+                            text={el.text}
+                            details={el.details}
+                        />
+                    ))}
+                </div>
             </div>
         </MainLayout>
     );
